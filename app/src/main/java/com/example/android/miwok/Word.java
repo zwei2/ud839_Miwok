757b52f0485fc068cbdcdb4d07539ff8d5558dce
Young <zwei2@naver.com>
2018-11-16 02:13:04 +0900

    Make custom class

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,30 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+
+public class Word {
+
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public String getmDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public void setmDefaultTranslation(String mDefaultTranslation) {
+        this.mDefaultTranslation = mDefaultTranslation;
+    }
+
+    public String getmMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public void setmMiwokTranslation(String mMiwokTranslation) {
+        this.mMiwokTranslation = mMiwokTranslation;
+    }
+}
